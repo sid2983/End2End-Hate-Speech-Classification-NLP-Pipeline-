83c404d9d73a28e1885cac797986caf01e87f40f
sid2983 <sid24000576@gmail.com>
2024-10-05 16:43:59 +0530

    mirrored strategy removed due to conflict

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -44,71 +44,70 @@ class ModelTrainer:
                 mlflow.log_param("batch_size", self.trainer_config.batch_size)
                 mlflow.log_param("learning_rate", self.trainer_config.learning_rate)
             
-                strategy = tf.distribute.MirroredStrategy()
-                logging.info(f"Number of devices: {strategy.num_replicas_in_sync}")
+                
 
-                with strategy.scope():
-                    # Load the transformed datasets
-                    train_data = self.load_dataset(self.trainer_config.transformed_train_data_path)
-                    test_data = self.load_dataset(self.trainer_config.transformed_test_data_path)
-                    
-                    # Load the pre-trained Roberta model for sequence classification
-                    model = TFRobertaForSequenceClassification.from_pretrained('roberta-base', num_labels=self.trainer_config.num_labels)
-                    
-                    # Define optimizer, loss, and metrics
-                    optimizer = tf.keras.optimizers.Adam(learning_rate=2e-5, epsilon=1e-08)
-                    loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-                    metric = tf.keras.metrics.SparseCategoricalAccuracy('accuracy')
-                    
-                    # Compile the model
-                    model.compile(optimizer=optimizer, loss=loss, metrics=[metric])
+                
+                # Load the transformed datasets
+                train_data = self.load_dataset(self.trainer_config.transformed_train_data_path)
+                test_data = self.load_dataset(self.trainer_config.transformed_test_data_path)
+                
+                # Load the pre-trained Roberta model for sequence classification
+                model = TFRobertaForSequenceClassification.from_pretrained('roberta-base', num_labels=self.trainer_config.num_labels)
+                
+                # Define optimizer, loss, and metrics
+                optimizer = tf.keras.optimizers.Adam(learning_rate=2e-5, epsilon=1e-08)
+                loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+                metric = tf.keras.metrics.SparseCategoricalAccuracy('accuracy')
+                
+                # Compile the model
+                model.compile(optimizer=optimizer, loss=loss, metrics=[metric])
 
-                    # TensorBoard and Model Checkpoint callbacks
-                    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=self.trainer_config.log_dir)
-                    checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(filepath=self.trainer_config.checkpoint_path, 
-                                                                            save_best_only=True, 
-                                                                            monitor='val_accuracy', 
-                                                                            mode='max')
-                    
-                    # Start training the model
-                    history = model.fit(
-                        train_data,
-                        validation_data=test_data,
-                        epochs=self.trainer_config.epochs,
-                        callbacks=[tensorboard_callback, checkpoint_callback]
-                    )
+                # TensorBoard and Model Checkpoint callbacks
+                tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=self.trainer_config.log_dir)
+                checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(filepath=self.trainer_config.checkpoint_path, 
+                                                                        save_best_only=True, 
+                                                                        monitor='val_accuracy', 
+                                                                        mode='max')
+                
+                # Start training the model
+                history = model.fit(
+                    train_data,
+                    validation_data=test_data,
+                    epochs=self.trainer_config.epochs,
+                    callbacks=[tensorboard_callback, checkpoint_callback]
+                )
 
-                    for epoch in range(self.trainer_config.epochs):
-                        mlflow.log_metric("train_loss", history.history['loss'][epoch], step=epoch)
-                        mlflow.log_metric("train_accuracy", history.history['accuracy'][epoch], step=epoch)
-                        mlflow.log_metric("val_loss", history.history['val_loss'][epoch], step=epoch)
-                        mlflow.log_metric("val_accuracy", history.history['val_accuracy'][epoch], step=epoch)
+                for epoch in range(self.trainer_config.epochs):
+                    mlflow.log_metric("train_loss", history.history['loss'][epoch], step=epoch)
+                    mlflow.log_metric("train_accuracy", history.history['accuracy'][epoch], step=epoch)
+                    mlflow.log_metric("val_loss", history.history['val_loss'][epoch], step=epoch)
+                    mlflow.log_metric("val_accuracy", history.history['val_accuracy'][epoch], step=epoch)
 
 
-                    # Evaluate the model on the test set
-                    eval_results = model.evaluate(test_data)
-                    loss_value, accuracy_value = eval_results[0], eval_results[1]
-                    logging.info(f"Evaluation results: {eval_results}")
+                # Evaluate the model on the test set
+                eval_results = model.evaluate(test_data)
+                loss_value, accuracy_value = eval_results[0], eval_results[1]
+                logging.info(f"Evaluation results: {eval_results}")
 
-                    mlflow.log_metric("test_loss", loss_value)
-                    mlflow.log_metric("test_accuracy", accuracy_value)
-                    
-                    # Save the final model
-                    model.save_pretrained(self.trainer_config.model_save_path)
-                    logging.info("Model saved successfully")
+                mlflow.log_metric("test_loss", loss_value)
+                mlflow.log_metric("test_accuracy", accuracy_value)
+                
+                # Save the final model
+                model.save_pretrained(self.trainer_config.model_save_path)
+                logging.info("Model saved successfully")
 
-                    mlflow.tensorflow.log_model(tf_saved_model_dir=self.trainer_config.model_save_path, 
-                                                artifact_path="model", 
-                                                registered_model_name=self.trainer_config.registered_model_name)
-                    
-                    mlflow.log_artifacts(self.trainer_config.log_dir, artifact_path="tensorboard_logs")
+                mlflow.tensorflow.log_model(tf_saved_model_dir=self.trainer_config.model_save_path, 
+                                            artifact_path="model", 
+                                            registered_model_name=self.trainer_config.registered_model_name)
+                
+                mlflow.log_artifacts(self.trainer_config.log_dir, artifact_path="tensorboard_logs")
 
-                    logging.info("Model Training Completed")
+                logging.info("Model Training Completed")
 
-                    model_uri = f"runs:/{run.info.run_id}/model"
-                    mlflow.register_model(model_uri=model_uri, name=self.trainer_config.registered_model_name)
-                    logging.info(f"Model registered under name: {self.trainer_config.registered_model_name}")
-                    return eval_results
+                model_uri = f"runs:/{run.info.run_id}/model"
+                mlflow.register_model(model_uri=model_uri, name=self.trainer_config.registered_model_name)
+                logging.info(f"Model registered under name: {self.trainer_config.registered_model_name}")
+                return eval_results
 
         except Exception as e:
             logging.error("Model Training Failed")
